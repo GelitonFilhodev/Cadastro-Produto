9a1c7c04431c2aa53faa2ff6b0870afe8814856f
Geliton <gelitonfilhodev@gmail.com>
2026-06-15 17:32:18 -0300

    Atualizando o database

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise")
 
 const conexao = mysql.createPool({
-    host: "localhost",
-    user: "geliton",
-    password: "1234",
-    database: "loja"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 module.exports = conexao
