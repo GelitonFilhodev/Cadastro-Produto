6f46d6877accf16a3789b2d6ce6fcbbca0603a9a
Geliton <gelitonfilhodev@gmail.com>
2026-06-15 19:19:48 -0300

    Integrando Front-End básico

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const database = require("./config/database")
 const routes = require("./routes/produtoRoute")
+const cors = require("cors")
 
 const app = express();
 
 app.use(express.json())
-
+app.use(cors())
 app.use(routes)
 
 app.listen(3001, () => {
